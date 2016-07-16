19167bc1368bc6e50f20958fee4a22952ac83213
Tom Harrington <tph@atomicbird.com>
2016-07-16 16:11:27 -0600

    Initial Commit

---
+++ MapDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MapDemo
+//
+//  Created by Tom Harrington on 7/16/16.
+//  Copyright © 2016 Atomic Bird LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
